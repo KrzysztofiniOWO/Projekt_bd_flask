65e79da2a2634e8c103a0b7aca35097bd1690779
Admin <molakrzysztof@gmail.com>
2024-01-13 13:26:05 +0100

    Added safe password policies

--- forms.py
+++ forms.py
@@ -1,15 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
-from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError, Regexp
 from helpers import is_username_available, is_email_available, is_phone_number_available
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
     email = StringField('Email', validators=[InputRequired(), Length(max=120)])
     phone_number = StringField('Phone Number', validators=[InputRequired(), Length(min=1, max=20)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=60)])
-    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password', message='Passwords must match')])
-    authentication_type = SelectField('Second Authentication Type', choices=[('second_email', 'Support email'), ('phone', 'SMS code'), ('authy_app', 'Authy application')], validators=[InputRequired()])
+    
+    password_policy_message = "Password must be at least 8 characters long, and contain at least one lowercase letter, one uppercase letter, one digit, and one special character"
+
+    password = PasswordField('Password', validators=[
+        InputRequired(),
+        Length(min=8, max=60, message="Password must be at least 8 characters long"),
+        Regexp(
+            regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:<>?~])",
+            message=password_policy_message
+        )
+    ])
+
+    confirm_password = PasswordField('Confirm Password', validators=[
+        InputRequired(),
+        EqualTo('password', message='Passwords must match')
+    ])
+
+    authentication_type = SelectField('Second Authentication Type', choices=[
+        ('second_email', 'Support email'),
+        ('phone', 'SMS code'),
+        ('authy_app', 'Authy application')],
+        validators=[InputRequired()]
+    )
+    
     terms_of_service_consent = BooleanField('I accept the Terms of Service.')
     newsletter_consent = BooleanField('I want to receive the Newsletter.')
     location_processing_consent = BooleanField('I consent to the processing of my location data to find the nearest kebab places.')
